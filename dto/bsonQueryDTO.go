1d0437621f079aef8692bf759b9f6e2be940ff05
Gurjinder Sidhu <gurjindersr@gmail.com>
2020-04-12 17:47:04 +0530

    integrated with mongo

--- dto/bsonQueryDTO.go
+++
@@ -1,5 +0,0 @@
-package dto
-
-type BsonQueryDTO struct {
-	BsonMapObject map[string]interface{}
-}
--- services/MongoService.go
+++ services/MongoService.go
@@ -4,7 +4,6 @@ import (
 	"blog_backend/configuration"
 	"blog_backend/dto"
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 
@@ -28,14 +27,9 @@ func executeSearch(db string, collection string, searchRequest dto.SearchRequest
 	fmt.Println(bsonSearchRequest)
 	var document bson.M
 	// Create a string using ` string escape ticks
-	query11 := `{"username":{$eq:"pappu"}}`
-	fmt.Println(query11)
-	var bsonMap bson.M
-	err := json.Unmarshal([]byte(query11), &bsonMap)
-	fmt.Println(bsonMap)
 	// Declare an empty BSON Map object
 
-	err = configuration.GetCollection(db, collection).FindOne(configuration.Ctx(), bsonMap).Decode(&document)
+	err := configuration.GetCollection(db, collection).FindOne(configuration.Ctx(), bsonSearchRequest).Decode(&document)
 	if err != nil {
 		fmt.Println("rrrrrrr")
 		log.Fatal(err)

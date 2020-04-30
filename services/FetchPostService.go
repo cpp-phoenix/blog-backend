7598d6a4e91877127e656df27907f6545c4d54ec
Gurjinder Sidhu <gurjindersr@gmail.com>
2020-04-30 16:50:36 +0530

    added pagination support in mongo

--- services/FetchPostService.go
+++ services/FetchPostService.go
@@ -8,7 +8,6 @@ import (
 )
 
 func FetchPost(postSearch dto.PostSearch) dto.UserResponse {
-	var posts []dto.PostDetails
 	var searchRequest bson.M
 	bsonResponse := executeSearchFetchMultiple(properties.BLOG_BACKEND_DATABASE, properties.POST_DETAILS_COLLECTION, searchRequest, "UpdatedTimeStamp", int64(postSearch.Page), int64(postSearch.Size))
 	var response dto.UserResponse
